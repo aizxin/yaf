e78af150ba13504fcbc5b23440177db5e95c05e7
aizxin <aizxin@gmail.com>
2017-09-02 13:37:19 +0800

    wang.yaf

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -7,9 +7,9 @@ class IndexController extends Yaf\Controller_Abstract {
 
     public function indexAction() {
         var_dump(\Yaf\Registry::get('config'));
-        \Yaf\Registry::get('cache')->save("my-data", [1, 2, 3, 4, 5]);
-        \Yaf\Registry::get('session')->set("data1", [1, 2, 3, 4, 5]);
-        \Yaf\Registry::get('log')->info('ddddsss');
+        // \Yaf\Registry::get('cache')->save("my-data", [1, 2, 3, 4, 5]);
+        // \Yaf\Registry::get('session')->set("data1", [1, 2, 3, 4, 5]);
+        // \Yaf\Registry::get('log')->info('ddddsss');
         // $this->display("index");
     }
 }
