afe8728952d2e94afbb71007ac72665f604581b7
aizxin <aizxin@gmail.com>
2017-09-02 17:44:35 +0800

    wang.admin

---
+++ application/modules/admin/controllers/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @name IndexController
+ * @author 空
+ */
+class IndexController extends Yaf\Controller_Abstract {
+
+    public function indexAction() {
+        // var_dump(\Yaf\Registry::get('config'));
+        // \Yaf\Registry::get('cache')->save("my-data", [1, 2, 3, 4, 5]);
+        // \Yaf\Registry::get('session')->set("data1", [1, 2, 3, 4, 5]);
+        // \Yaf\Registry::get('log')->info('ddddsss');
+        $this->display("index");
+    }
+}
